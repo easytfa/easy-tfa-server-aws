81f336683e886bbab3db9c4b165dc1914f64c333
Mattis Turin-Zelenko <mattis.turin-zelenko@chax.at>
2022-04-02 03:08:02 +0200

    Add DynamoDb expirationTime
    closes #1

--- src/handler/websocketLinking.ts
+++ src/handler/websocketLinking.ts
@@ -9,18 +9,21 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
   const data = body.data;
 
   const hashHex = crypto.createHash('sha256').update(data).digest('hex');
+  const expirationTime = Math.round(new Date().getTime() / 1000 + 24 * 3600);
   await Promise.all([
     Db.put({
       TableName: process.env.DYNAMODB_TABLE_NAME!,
       Item: {
         primaryKey: `public-key#${hashHex}`,
         publicKey: data,
+        expirationTime: expirationTime,
       },
     }), Db.put({
       TableName: process.env.DYNAMODB_TABLE_NAME!,
       Item: {
         primaryKey: `websocket-client#${hashHex}`,
         connectionId: connectionId,
+        expirationTime: expirationTime,
       },
     }),
   ]);
--- src/handler/websocketQuery.ts
+++ src/handler/websocketQuery.ts
@@ -7,18 +7,21 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
   const body = JSON.parse(event.body!);
   const data = body.data;
 
+  const expirationTime = Math.round(new Date().getTime() / 1000 + 24 * 3600);
   await Promise.all([
     Db.put({
       TableName: process.env.DYNAMODB_TABLE_NAME!,
       Item: {
         primaryKey: `code-query#${data.hash}`,
         message: data.message,
+        expirationTime: expirationTime,
       },
     }), Db.put({
       TableName: process.env.DYNAMODB_TABLE_NAME!,
       Item: {
         primaryKey: `websocket-client#${data.hash}`,
         connectionId: connectionId,
+        expirationTime: expirationTime,
       },
     }),
   ]);
